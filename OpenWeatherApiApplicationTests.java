213182bbcc44f50ddbda79ece45105f51e5137f9
durgesh07-git <durgeshsingh10b@gmail.com>
2025-01-09 07:27:20 +0530

    Add files via upload

---
+++ OpenWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kukilej.openweatherapi.openweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
